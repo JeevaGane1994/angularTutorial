e4cf592c66a50f8da4ee89228767efb778413bde
Jeeva G <j.ganesh@dfarminc.com>
2021-01-19 23:25:03 +0530

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  title ='Angular'
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
+
+
+
+
+//copmponnet
+
+
+// we need to include html, css
