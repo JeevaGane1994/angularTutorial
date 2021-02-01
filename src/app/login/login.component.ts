c389ee83c614350b2b249a440e10cae11973c7ce
Jeeva G <j.ganesh@dfarminc.com>
2021-02-01 20:35:57 +0530

    Added Reactive form for th elogin component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,25 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  templateUrl:'./login.component.html',
+  templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  implements OnInit {
 
-  title ='Angular'
+  loginForm:FormGroup; 
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl(""),
+      pwd: new FormControl("")
+    });
 
+  }
+ 
+  onLogin() {
+    const data = this.loginForm.value
+    console.log(data);
   }
 
 }
-
-
-
-
-//copmponnet
-
-
-// we need to include html, css
