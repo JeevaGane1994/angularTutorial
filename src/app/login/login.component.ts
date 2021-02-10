27383758d9ec2ae8136fb40473e1148df15313ed
Jeeva G <j.ganesh@dfarminc.com>
2021-02-10 10:59:47 +0530

    added routing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,31 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms'; 
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent implements OnInit {
 
-  loginForm:FormGroup; 
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private rout:Router) { }   
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
       emailid: new FormControl("", Validators.compose([
         Validators.required,
         Validators.pattern("[^ @]*@[^ @]*")
-     ])),
+      ])),
       pwd: new FormControl("")
     });
 
   }
- 
+
   onLogin() {
-    const result = this.loginForm.valid // valid, invalid, dirty, prstine, touchec, untoched
-    console.log(result);
+    const result = this.loginForm.valid; // valid, invalid, dirty, prstine, touchec, untoched
+    const data = this.loginForm.value; //{emailid:'test@gmail.com', pwd:'admin'} 
+    if (result) {
+      if (data.emailid == 'test@gmail.com' && data.pwd === 'admin') {
+      this.rout.navigate(['/pages/dashboard'])
+      }
+    }
+  }
+
+  navigateToSignup(){
+    this.rout.navigate(['/signup'])
   }
 
 }
